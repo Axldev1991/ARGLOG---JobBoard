ca4b4458c296b01728281a840dd228ef2fb759f9
Axel <castellanoaxl@gmail.com>
2026-01-10 15:54:12 -0300

    feat: Implement register functionality with server actions

---
+++ src/actions/register.ts
@@ -0,0 +1,33 @@
+"use server" // <--- ¡MAGIA! Esto convierte la función en un API invisible
+import { prisma } from "@/lib/db"
+import { hash } from "bcryptjs"
+import { redirect } from "next/navigation"
+
+export async function registerUser(formData: FormData) {
+    // 1. Convertir FormData a Objeto simple
+    const name = formData.get("name") as string
+    const email = formData.get("email") as string
+    const password = formData.get("password") as string
+    const role = formData.get("role") as string
+
+    // 2. Validar que no falte nada (Basic)
+    if (!name || !email || !password || !role) {
+        console.log("Faltan datos")
+        return
+    }
+
+    // 3. Crear el usuario en la DB
+    // Await es clave: esperamos a la DB, y hasheamos la pass (10 rondas de seguridad)
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password: await hash(password, 10), 
+            role
+        }
+    })
+
+    console.log("Usuario creado!")
+    // 4. Redirigir al Login
+    redirect("/login")
+}
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { registerUser } from "@/actions/register";
 
 export default function RegisterPage() {
     const [role, setRole] = useState("candidate"); // Por defecto es candidato
@@ -11,11 +12,12 @@ export default function RegisterPage() {
                 <h1 className="text-4xl font-bold">
                     Quien eres?
                 </h1>
-                <form action="" className="flex flex-col gap-4">
+                <form action={registerUser} className="flex flex-col gap-4">
                     <Input type="text" placeholder="Nombre" />
                     <Input type="email" placeholder="Email" />
                     <Input type="password" placeholder="Contraseña" />
                     <div className="flex gap-4">
+                        <input type="hidden" name="role" value={role} />
                         {/* Botón Candidato */}
                         <Button
                             type="button" // Importante: evita que envíe el formulario
@@ -34,6 +36,9 @@ export default function RegisterPage() {
                             className="w-full"
                         >
                             Soy Empresa
+                        </Button>
+                        <Button type="submit" className="w-full mt-4">
+                            Registrarse
                         </Button>
                     </div>
                 </form>
